76874dca847e3f4e3c6c048c64d822db0536305e
mohamedit-hash <mohamed.a.allouache@gmail.com>
2025-05-08 13:37:11 +0100

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,14 @@
+import mongoose from "mongoose";
 
+import "dotenv/config";
+
+export default async function connectDB() {
+    const URL = "mongodb+srv://moh:1996633@cluster0.zaqwjjt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"||process.env.MONGO_DB_URL;"
+          try {
+        await mongoose.connect(URL
+        );
+        console.log("MongoDB connected successfully");
+    } catch (error) {
+        console.log("Error connecting to MongoDB: ", error.message);
+    }
+}
