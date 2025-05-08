695b2169e3e6da8bd23b7c243afc9c1d3ba42b88
mohamedit-hash <mohamed.a.allouache@gmail.com>
2025-05-08 13:51:28 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
