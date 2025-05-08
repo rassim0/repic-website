73ef43dc880e0f4c907c3eefa1db68ec3ceea2df
mohamedit-hash <mohamed.a.allouache@gmail.com>
2025-05-08 13:42:40 +0100

    Update auth.js

--- backend/controller/auth.js
+++ backend/controller/auth.js
@@ -1,1 +1,47 @@
+import User from "../models/user.js"; // link ykon s7i7
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
+export async function login (req,res) {
+ try {
+      const {email,password} = req.body;
+      const user = await User.findOne({email});
+      if (user){
+        const isMatch = await bcrypt.compare(password ,user.password)
+        if(isMatch) {
+            const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"30d"});
+            delete user._doc.password;
+       return res.status(200).json({message:"login is successfull",datas:{token,...user._doc}})
+       
+        }else{
+            return res.status(400).json({message:"Invalid credentials"})
+        }
+      }
+      return res.status(400).json({message:"Invalid credentials"})
+        
+ } catch (error) {
+    console.log(error);
+      res.status(500).json({message:error.message});
+    
+ }
+}
+//hadi mazalt
+/*
+export async function register(req,res){
+ try {
+      const {name,email,password} = req.body;
+      const existingUser = await User.findOne({email});
+     if(existingUser){
+         res.status(400).json ({message:"Email already exists"});
+}
+*/
+const hashedPassword = await bcrypt.hash(password,10)
+const user = new User({name,email,password:hashedPassword})
+await user.save();
+    delete user._doc.password;
+    res.status(201).json({data:user ,message:"Your Account Created Successfully"})
+ } catch (error) {
+    console.log(error);
+      res.status(500).json({message:error.message});
+ }
+}
